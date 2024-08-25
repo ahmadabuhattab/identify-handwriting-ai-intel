d1b6ad9445044b26585c38f5836ce0b106467a15
Ahmad Abu-Hattab <101012392+ahmadabuhattab@users.noreply.github.com>
2024-08-25 17:24:36 -0400

    Create data_preprocessing.py

---
+++ src/data_preprocessing.py
@@ -0,0 +1,22 @@
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+
+def load_and_preprocess_data():
+    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+    x_train = x_train.reshape(-1, 28, 28, 1)
+    x_test = x_test.reshape(-1, 28, 28, 1)
+    y_train = tf.keras.utils.to_categorical(y_train, 10)
+    y_test = tf.keras.utils.to_categorical(y_test, 10)
+    return (x_train, y_train), (x_test, y_test)
+
+def create_data_augmentor():
+    return ImageDataGenerator(
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        shear_range=0.1,
+        zoom_range=0.1,
+        horizontal_flip=False
+    )
